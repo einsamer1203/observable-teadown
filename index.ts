3c26d2e124158f2d876acaf85f96fe1967605210
einsamer1203 <einsamer1203@gmail.com>
2021-10-26 13:37:54 -0400

    Init Observable

--- index.ts
+++ index.ts
@@ -1,6 +1,36 @@
-// Import stylesheets
-import './style.css';
+interface Observer<T> {
+  next(value: T): void;
+  error(err: any): void;
+  complete(): void;
+}
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+type Teardown = () => void;
+
+class Observable<T> {
+  constructor(private init: (observer: Observer<T>) => Teardown) {}
+
+  subscribe(observer: Observer<T>) {
+    return this.init(observer);
+  }
+}
+
+const myOnservable = new Observable((observer: Observer<number>) => {
+  let i = 1;
+
+  const id = setInterval(() => {
+    observer.next(i++);
+  }, 1000);
+
+  return () => {
+    clearInterval(id);
+  };
+});
+
+const teardown = myOnservable.subscribe({
+  next: (d) => console.log(d),
+  error: (err) => console.error(err),
+  complete: () => console.log('completed!'),
+});
+setTimeout(() => {
+  teardown();
+}, 5001);
